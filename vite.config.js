0fe4a98d8cd74e0de906ec8930dff194472182cb
mac <mac@MacBook-Pro-mac.local>
2025-05-20 00:53:50 +0300

    Новий деплой з виправленими зображеннями

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/charge-tap-landing/' // Базовий шлях для GitHub Pages (має відповідати назві репозиторію)
-})
